b08eadce6ca712f112f1b639e7528c880e4a71c4
DavidKim <davidKim>
2026-01-20 10:24:38 -0500

    Entity 상속 기능

--- src/main/kotlin/com/tsh/starter/be/bespringbootserver/dao/jpa/gnJsonMsgSpec/entity/GnJsonMsgSpecEntity.kt
+++ src/main/kotlin/com/tsh/starter/be/bespringbootserver/dao/jpa/gnJsonMsgSpec/entity/GnJsonMsgSpecEntity.kt
@@ -1,6 +1,7 @@
 package com.tsh.starter.be.bespringbootserver.dao.jpa.gnJsonMsgSpec.entity
 
 import com.tsh.starter.be.bespringbootserver.constant.ApTableName
+import com.tsh.starter.be.bespringbootserver.dao.jpa.common.BaseEntity
 import com.tsh.starter.be.bespringbootserver.dao.jpa.common.GeneratedCustomId
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
@@ -12,20 +13,18 @@ import jakarta.persistence.UniqueConstraint
     schema = "public",
     uniqueConstraints = [UniqueConstraint(name = "uk_gn_json_msg_spec_01", columnNames = ["msg_nm", "element_nm"])]
     )
-class GnJsonMsgSpecEntity {
+class GnJsonMsgSpecEntity(
 
     @Id
     @GeneratedCustomId
-    var objId: String? = null
+    var objId: String? = null,
 
-    var srv_nm : String? = null
-    var msg_nm : String? = null
-    var element_nm : String? = null
-    var data_typ : String? = null
-    var prnt_element : String? = null
-    var description: String? = null
+    var srv_nm : String? = null,
+    var msg_nm : String? = null,
+    var element_nm : String? = null,
+    var data_typ : String? = null,
+    var prnt_element : String? = null,
+    var description: String? = null,
     var sample_val: String? = null
 
-
-
-}
+): BaseEntity()
