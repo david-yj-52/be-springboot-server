ba700332c58f891245a38ab7135b1b5936d872bc
DavidKim <davidKim>
2026-01-25 13:30:05 -0500

    빌드 파일 배포 기능 개발

--- src/main/kotlin/com/tsh/starter/be/bespringbootserver/model/intf/in/SampleBodyIvo.kt
+++ src/main/kotlin/com/tsh/starter/be/bespringbootserver/model/intf/in/SampleBodyIvo.kt
@@ -1,10 +1,7 @@
 package com.tsh.starter.be.bespringbootserver.model.intf.`in`
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.tsh.starter.be.bespringbootserver.constant.InterfaceSystemType
 import com.tsh.starter.be.bespringbootserver.model.intf.common.ApBodyIvo
-import com.tsh.starter.be.bespringbootserver.model.intf.common.ApInterfaceVo
-import com.tsh.starter.be.bespringbootserver.model.intf.common.toApHeadVo
 
 data class SampleBodyIvo(
     val status: String,
@@ -19,21 +16,21 @@ data class SampleBodyIvo(
     }
 }
 
-fun main() {
-
-    val objectMapper = jacksonObjectMapper()
-
-    val body = SampleBodyIvo(
-        status = "Active",
-        version = "1.0",
-        path = "/sameple",
-        siteId = "Sample",
-        userId = "user"
-    )
-
-    val head = body.toApHeadVo(src = SampleBodyIvo.SRC)
-
-    val ivo = ApInterfaceVo(head, body)
-
-    println(objectMapper.writeValueAsString(ivo))
-}
+//fun main() {
+//
+//    val objectMapper = jacksonObjectMapper()
+//
+//    val body = SampleBodyIvo(
+//        status = "Active",
+//        version = "1.0",
+//        path = "/sameple",
+//        siteId = "Sample",
+//        userId = "user"
+//    )
+//
+//    val head = body.toApHeadVo(src = SampleBodyIvo.SRC)
+//
+//    val ivo = ApInterfaceVo(head, body)
+//
+//    println(objectMapper.writeValueAsString(ivo))
+//}
